49ab1b47e9f8ac4f422aac25e6b2b17b26a9860a
Stephan Bischoff <stephan.bischoff@digle.net>
2020-12-02 15:51:24 +0100

    Implemented Intervall Parsing

---
+++ 2020/day02_1/src/interval.rs
@@ -0,0 +1,109 @@
+use std::convert::From;
+use std::error::Error;
+use std::fmt;
+use std::num::ParseIntError;
+use std::str::FromStr;
+
+#[derive(Debug, PartialEq)]
+pub enum ParseIntervalError {
+    WrongComponentOrder,
+    WrongComponentNumber,
+    IntegerError(ParseIntError),
+}
+
+impl Error for ParseIntervalError {
+    fn source(&self) -> Option<&(dyn Error + 'static)> {
+        match self {
+            Self::IntegerError(ref pie) => Some(pie),
+            _ => None,
+        }
+    }
+}
+
+impl fmt::Display for ParseIntervalError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            Self::WrongComponentNumber => write!(f, "Wrong number of interval components"),
+            Self::WrongComponentOrder => write!(f, "Wrong order of components"),
+            Self::IntegerError(ie) => write!(f, "Interval parsing failed: {}", ie),
+        }
+    }
+}
+
+impl From<ParseIntError> for ParseIntervalError {
+    fn from(pie: ParseIntError) -> Self {
+        Self::IntegerError(pie)
+    }
+}
+
+#[derive(Debug, PartialEq)]
+pub struct Interval {
+    min: u32,
+    max: u32,
+}
+
+impl FromStr for Interval {
+    type Err = ParseIntervalError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let split = s.split('-');
+        let mut components = Vec::new();
+
+        // sadly parsing in a maping of the split does not allow reasonable
+        // error handling :(
+        for i in split {
+            components.push(i.parse::<u32>()?);
+        }
+
+        if components.len() == 2 {
+            if components[0] <= components[1] {
+                return Ok(Self {
+                    min: components[0],
+                    max: components[1],
+                });
+            }
+            return Err(ParseIntervalError::WrongComponentOrder);
+        }
+        Err(ParseIntervalError::WrongComponentNumber)
+    }
+}
+
+#[test]
+fn interval_from_str_valid() {
+    let input = "1-2";
+    let expected = Ok(Interval { min: 1, max: 2 });
+    let result = Interval::from_str(input);
+    assert_eq!(expected, result);
+}
+
+#[test]
+fn interval_from_str_wrong_order() {
+    let input = "2-1";
+    let expected = Err(ParseIntervalError::WrongComponentOrder);
+    let result = Interval::from_str(input);
+    assert_eq!(expected, result);
+}
+
+#[test]
+fn interval_from_str_wrong_format() {
+    let input = "2+1";
+    let result = Interval::from_str(input);
+
+    assert!(match result {
+        Err(ParseIntervalError::IntegerError(_)) => true,
+        _ => false,
+    });
+}
+
+#[test]
+fn interval_from_str_wrong_number() {
+    let input = "1-2-3";
+    let expected = Err(ParseIntervalError::WrongComponentNumber);
+    let result = Interval::from_str(input);
+    assert_eq!(expected, result);
+
+    let input = "1";
+    let expected = Err(ParseIntervalError::WrongComponentNumber);
+    let result = Interval::from_str(input);
+    assert_eq!(expected, result);
+}
