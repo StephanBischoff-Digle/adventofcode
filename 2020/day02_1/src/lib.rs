ab82a4c41b92906e9fa09dc24cf7152b4fa14c03
Stephan Bischoff <stephan.bischoff@digle.net>
2020-12-02 18:53:24 +0100

    First implementation of the solution

--- 2020/day02_1/src/lib.rs
+++ 2020/day02_1/src/lib.rs
@@ -1,2 +1,23 @@
 pub mod interval;
 pub mod policy;
+
+use std::str::FromStr;
+
+pub fn solve(input: &Vec<&str>) -> usize {
+    input
+        .iter()
+        .map(|i| {
+            let split: Vec<&str> = i.split(":").collect();
+            let policy = policy::Policy::from_str(split[0]).unwrap();
+            let data = split[1];
+            (policy, data)
+        })
+        .filter(|(policy, data)| policy.apply(data))
+        .count()
+}
+
+#[test]
+fn aoc_example() {
+    let input = vec!["1-3 a: abcde", "1-3 b: cdefg", "2-9 c: ccccccccc"];
+    assert_eq!(2, solve(&input));
+}
