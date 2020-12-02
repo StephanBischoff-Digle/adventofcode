a4dacb00ce767de140396c5ec6c7406dfd1f0995
Stephan Bischoff <stephan.bischoff@digle.net>
2020-12-02 18:56:15 +0100

    Done Day 2 Star 1

---
+++ 2020/day02_1/src/bin/main.rs
@@ -0,0 +1,11 @@
+use day02_1::solve;
+use std::fs;
+
+fn main() {
+    println!("Reading input");
+
+    let input = fs::read_to_string("input").expect("Failed to read input file");
+    let input: Vec<&str> = input.trim_end().split("\n").collect();
+
+    println!("Solution: {}", solve(&input));
+}
