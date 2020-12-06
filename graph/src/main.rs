b5837ba137a848b626b98b5f1f5541525e1fab5b
Stephan Bischoff <stephan.bischoff@digle.net>
2020-12-06 23:50:47 +0100

    Added some comments to the graph program

--- graph/src/main.rs
+++ graph/src/main.rs
@@ -14,15 +14,20 @@ enum Part {
 }
 
 fn main() {
+    // Some config stuff
+    // TODO: read from config file to render more dynamic
     let border: i32 = 30;
     let linespacing: i32 = 20;
     let fontsize: i32 = 10;
     let mark_radius: i32 = (fontsize as f32 * 0.66).round() as i32;
     let column_space: i32 = 100;
 
+    // Read data file
     let input = fs::read_to_string("data.yml").expect("Failed to read input file");
     let data: BTreeMap<u32, BTreeMap<usize, Vec<Part>>> = serde_yaml::from_str(&input).unwrap();
 
+    ////////////////////////////////////////////////////////////////////////
+    // Setup SVG root element
     let mut document = Document::new()
         .set(
             "viewBox",
@@ -36,14 +41,19 @@ fn main() {
         .set("width", border + data.len() as i32 * column_space)
         .set("height", border + (fontsize + linespacing) * 25 + border);
 
+    // Set background to white
     document = document.add(
         Rectangle::new()
             .set("width", "100%")
             .set("height", "100%")
             .set("fill", "white"),
     );
+
+    ////////////////////////////////////////////////////////////////////////
+    // Render data content
     for (i, (year, days)) in data.iter().enumerate() {
         let i = i as i32;
+
         let year_txt = svg::node::element::Text::new()
             .add(Text::new(format!("{}", year)))
             .set("x", 2 * border + i * column_space)
@@ -52,6 +62,7 @@ fn main() {
             .set("font-size", 2 * fontsize);
         document = document.add(year_txt);
 
+        // Draw all the circles
         for (d, parts) in days.iter() {
             let d = *d as i32;
             for part in parts.iter() {
@@ -71,6 +82,8 @@ fn main() {
         }
     }
 
+    ////////////////////////////////////////////////////////////////////////
+    // Draw the day number text on the left side
     for d in 1..=25 {
         let t = svg::node::element::Text::new()
             .add(Text::new(format!("{:02}", d)))
@@ -79,6 +92,7 @@ fn main() {
             .set("y", (border + (linespacing + fontsize) * d) as usize);
         document = document.add(t);
 
+        // black dashed line for each week
         if d % 7 == 0 {
             let line = Line::new()
                 .set("stroke", "black")
@@ -99,6 +113,7 @@ fn main() {
             document = document.add(line);
         }
 
+        // blue line every 5 days
         if d % 5 == 0 {
             let line = Line::new()
                 .set("stroke", "blue")
