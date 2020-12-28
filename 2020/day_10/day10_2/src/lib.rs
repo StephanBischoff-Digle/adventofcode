df30d6d077157f7d234b9fd6284ee32062f444ee
Stephan Bischoff <stephan.bischoff@digle.net>
2020-12-28 23:40:27 +0100

    Implemented more efficient solution
    Instead of matrix multiplication the new solution employs
    dynamic programming. This brings the time complexity down
    from O(n^3) to O(n log n) from the sorting algorithm. The bulk
    work is achived in linear O(n) time complexity.

--- 2020/day_10/day10_2/src/lib.rs
+++ 2020/day_10/day10_2/src/lib.rs
@@ -1,28 +1,4 @@
-fn matrix_mul(lhs: Vec<Vec<u64>>, rhs: &Vec<Vec<u64>>) -> Vec<Vec<u64>> {
-    let mut result = lhs.clone();
-
-    for i in 0..lhs.len() {
-        for j in 0..lhs[i].len() {
-            let mut sum = 0;
-            for k in 0..lhs.len() {
-                sum += lhs[i][k] * rhs[k][j];
-            }
-            result[i][j] = sum;
-        }
-    }
-
-    result
-}
-
-#[test]
-fn matrix_mul_test() {
-    let m = vec![vec![0, 1, 1], vec![0, 0, 1], vec![0, 0, 0]];
-    let k = m.clone();
-
-    let expected = vec![vec![0, 0, 1], vec![0, 0, 0], vec![0, 0, 0]];
-    let result = matrix_mul(m, &k);
-    assert_eq!(expected, result);
-}
+use std::collections::HashMap;
 
 pub fn solve(input: &str) -> u64 {
     let mut adapters: Vec<u64> = input
@@ -30,45 +6,27 @@ pub fn solve(input: &str) -> u64 {
         .lines()
         .map(|s| s.parse().expect("Failed to parse input"))
         .collect();
-    adapters.push(0);
-    adapters.sort();
-    // hack for the windows
-    adapters.push(adapters[adapters.len() - 1]);
-    adapters.push(adapters[adapters.len() - 1]);
-    adapters.push(adapters[adapters.len() - 1]);
+    adapters.sort_unstable();
 
-    // build adjacency matrix
-    let mut adj = Vec::with_capacity(adapters.len());
-    &adapters.windows(4).enumerate().for_each(|(i, window)| {
-        let mut row = Vec::new();
-        row.resize_with(adapters.len(), Default::default);
+    let max_jolts = adapters.last().unwrap_or(&0) + 3;
+    adapters.push(max_jolts);
+
+    let mut cache = HashMap::new();
+    cache.insert(0, 1);
+
+    for i in &adapters {
+        let mut sum = 0;
         for j in 1..=3 {
-            if window[j] - window[0] <= 3 {
-                row[i + j] = 1;
-            } else {
-                break;
+            if i >= &j {
+                sum += cache.get(&(i - j)).unwrap_or(&0);
             }
         }
-        adj.push(row);
-    });
-
-    // multiply
-    // TODO: This is super inefficent. `adj` is a strictly upper triangular matrix, there should be way to make this better
-    let original = adj.clone();
-    let mut paths = adj[0][adj[0].len() - 1];
-    for i in 0..adapters.len() {
-        println!(
-            "{:3}% done ({} out of {})",
-            (i * 100) / adapters.len(),
-            i,
-            adapters.len()
-        );
-        adj = matrix_mul(adj, &original);
-        paths += adj[0][adj[0].len() - 1];
+        cache.insert(*i, sum);
     }
 
-    paths
+    *cache.get(&max_jolts).unwrap()
 }
+
 
 #[test]
 fn example_a() {
