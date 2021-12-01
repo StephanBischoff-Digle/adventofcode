2d5118168f296ca59169af33bae7568b31b95454
Stephan Bischoff <stephan.bischoff@digle.net>
2021-12-01 15:14:21 +0100

    [add] rust solutions for day 1

---
+++ 2021/day_01/day01_1/d011/src/main.rs
@@ -0,0 +1,20 @@
+use std::fs;
+
+fn main() {
+    println!("Reading input");
+
+    let input = fs::read_to_string("input.txt").expect("Failed to read input.txt");
+    let input: Vec<u32> = input
+        .trim_end()
+        .split("\n")
+        .map(|l| l.parse::<u32>().expect("Failed to parse"))
+        .collect();
+
+    let result = &input
+        .windows(2)
+        .filter_map(|ab| (ab[0] < ab[1]).then(|| {}))
+        .collect::<Vec<_>>()
+        .len();
+
+    println!("{}", result);
+}
---
+++ 2021/day_01/day01_2/d012/src/main.rs
@@ -0,0 +1,27 @@
+use std::fs;
+
+fn main() {
+    println!("Reading input");
+
+    let input = fs::read_to_string("input.txt").expect("Failed to read input.txt");
+    let input: Vec<u32> = input
+        .trim_end()
+        .split("\n")
+        .map(|l| l.parse::<u32>().expect("Failed to parse"))
+        .collect();
+
+    let result = &input
+        .windows(4)
+        .filter_map(|ab| {
+            (if let Some(last) = ab.last() {
+                ab[0] < *last
+            } else {
+                false
+            })
+            .then(|| {})
+        })
+        .collect::<Vec<_>>()
+        .len();
+
+    println!("{}", result);
+}
