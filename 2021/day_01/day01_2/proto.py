5151bdfc4e90ae5a81f46dad4dad6202a55d21d2
Stephan Bischoff <stephan.bischoff@digle.net>
2021-12-01 15:15:58 +0100

    [imp] improved d01.2 prototype

--- 2021/day_01/day01_2/proto.py
+++ 2021/day_01/day01_2/proto.py
@@ -3,7 +3,5 @@
 import fileinput
 
 measures = [int(line.strip()) for line in fileinput.input("input.txt")]
-summs = [sum(measures[i : i + 3]) for i in range(len(measures) - 2)]
-
-solution = [summs[i] < summs[i + 1] for i in range(len(summs) - 1)].count(True)
+solution = [measures[i] < measures[i + 3] for i in range(len(measures) - 3)].count(True)
 print(solution)
