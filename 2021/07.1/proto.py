9312c5c9833f3b72c6340894efeb13f8db8359e3
Stephan Bischoff <stephan.bischoff@digle.net>
2021-12-07 17:54:03 +0100

    [add] naive 2021.07.1 prototype

---
+++ 2021/07.1/proto.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+import fileinput
+from collections import Counter
+
+test_input = [16, 1, 2, 0, 4, 2, 7, 1, 2, 14]
+
+
+def read_input() -> list[str]:
+    return [str(line.strip()) for line in fileinput.input("input.txt")]
+
+
+def find_min(lst: list[int]) -> int:
+    start = min(lst)
+    prev = sum(lst)
+    current = prev
+
+    while prev >= current:
+        start += 1
+        prev = current
+        current = sum([abs(start - l) for l in lst])
+
+    return prev
+
+
+in_lst = [int(x) for x in read_input()[0].split(",")]
+print(find_min(in_lst))
