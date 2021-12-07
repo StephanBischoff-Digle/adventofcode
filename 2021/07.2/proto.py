0579a1ecf242f7e90b1c8d62e15ac3620d7c3053
Stephan Bischoff <stephan.bischoff@digle.net>
2021-12-07 18:15:23 +0100

    [add] prototype for 2021.07.2

---
+++ 2021/07.2/proto.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+
+import fileinput
+from collections import Counter
+
+test_input = [16, 1, 2, 0, 4, 2, 7, 1, 2, 14]
+
+
+def read_input() -> list[str]:
+    return [str(line.strip()) for line in fileinput.input("input.txt")]
+
+
+def find_min(lst: list[int]) -> int:
+    # start = min(lst)
+    start = 5
+    prev = sum([((abs(start - l)) * (abs(start - l) + 1))/2 for l in lst])
+    current = prev
+
+    while prev >= current:
+        start += 1
+        prev = current
+        # hashtag gauss
+        current = sum(
+            [((abs(start - l)) * (abs(start - l) + 1))/2 for l in lst])
+
+    return int(prev)
+
+
+in_lst = [int(x) for x in read_input()[0].split(",")]
+print(find_min(in_lst))
