2eeeb6371df5d29de9dcb4b337df70a8d18e23a6
Stephan Bischoff <stephan.bischoff@mailo.eu>
2022-12-11 11:25:59 +0100

    [add] 2022.11.1+2 Python

---
+++ 2022/11/proto2.py
@@ -0,0 +1,99 @@
+from typing import Callable
+
+
+class Monkey(object):
+    def __init__(
+        self,
+        items: list[int],
+        op: Callable[[int], int],
+        test: int,
+        succ: int,
+        fail: int,
+    ):
+        self.items = items
+        self.op = op
+        self.test = test
+        self.succ = succ
+        self.fail = fail
+        self.factor = 0
+        self.inspected = 0
+
+    def compute_turn(self) -> list[tuple[int, int]]:
+        ret_list = []
+        while len(self.items) != 0:
+            self.inspected += 1
+            item = self.items.pop(0)
+            item = self.op(item)
+            item = item % self.factor
+            if item % self.test == 0:
+                ret_list.append((self.succ, item))
+            else:
+                ret_list.append((self.fail, item))
+        return ret_list
+
+
+def rounds(monkeys: list[Monkey], n: int, prints: list[int]) -> list[Monkey]:
+    for i in range(n):
+        for idx, monkey in enumerate(monkeys):
+            throws = monkey.compute_turn()
+            for target, item in throws:
+                monkeys[target].items.append(item)
+
+        if i+1 in prints:
+            print(f"== After round {i+1} ==")
+            for idx, monkey in enumerate(monkeys):
+                print(
+                    f"Monkey {idx}: inspected items {monkey.inspected} times.")
+
+    return monkeys
+
+
+def parse_op(fn_string: str) -> Callable[[int], int]:
+    op_str = fn_string.split("=")[1].strip()
+
+    if "+" in op_str:
+        if op_str.endswith("old"):
+            return lambda x: x + x
+        else:
+            v = int(op_str.split("+")[1])
+            return lambda x: x + v
+    else:
+        if op_str.endswith("old"):
+            return lambda x: x * x
+        else:
+            v = int(op_str.split("*")[1])
+            return lambda x: x * v
+
+
+def main() -> None:
+    monkeys: list[Monkey] = []
+
+    with open("input.txt", "r") as f:
+        in_data = f.read()
+
+        blocks = in_data.split("\n\n")
+        for block in blocks:
+            lines = block.split("\n")
+            items = [int(v) for v in lines[1].split(":")[1].split(",")]
+            op = parse_op(lines[2].split(":")[1])
+            test_n = int(lines[3].split("divisible by")[1])
+            target_succ = int(lines[4].split("monkey")[1])
+            target_fail = int(lines[5].split("monkey")[1])
+
+            monkeys.append(Monkey(items, op, test_n, target_succ, target_fail))
+
+    factor = 1
+    for monkey in monkeys:
+        factor = factor * monkey.test
+    for monkey in monkeys:
+        monkey.factor = factor
+
+    monkeys = rounds(monkeys, 10000, [1, 20, 1000, 10000])
+    inspected = [monkey.inspected for monkey in monkeys]
+    inspected.sort()
+    a, b = inspected[-2:]
+    print(f"{a} {b} {a * b}")
+
+
+if __name__ == "__main__":
+    main()
